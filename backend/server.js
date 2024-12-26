b1d1eeccac4bc912d50bac2782a9296e999ad2a9
prajwal240 <prajwal.dec7@gmail.com>
2024-12-26 23:37:33 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: process.env.FRONTEND_URL
     }
 });
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
